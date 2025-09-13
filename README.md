3ab26aa3fefbbc6f7b775d686714808c047d4309
Varalakshmi12 <pandu.chetan27@gmail.com>
2025-09-13 00:46:54 -0500

    presentation summary added in README.md file

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
-This is the README.md file! I, as the presenter, will fill it with content!
+This is the README.md file! I (Vara)as the presenter, will fill it with content!
+key scenarios tested:
+----------------------
+calculateDiscount function:
+
+valid discount(price=100,discountrate=0.2)
+invalid input(negative price)
+edge case(0% discount)
+
+filterProducts function:
+
+filtered products with valid input(amount>4)
+invalid input(non arrays and non callback function)
+edge case with no product match
+
+sortInventory function:
+
+sorted alphabetically and numarically.
+invalid keys.
+edge case with empty array.
+
+challenges faced:
+---------------------------
+In writing edge cases but later added few test cases.
+confusion that When to use .toBe ,.toEqual .
+when running test few tests are not passed.
+
+In sortInventory function first Inventory array is mutated ,to fix this we took copy of array using [...inventory] 
+
+Overall collabarion worked well to complete project.
--- testing/inventory.test.js
+++ testing/inventory.test.js
@@ -29,11 +29,11 @@ const AmtSort =  [{item: "lint catcher", amount: 2},
 
 describe('calculateDiscount',()=>{
     test('should be applied discount',()=>{
-        expect(inventory.calculateDiscount(100,0.2)).toEqual(80);
+        expect(inventory.calculateDiscount(100,0.2)).toEqual(80);//positive test case
     });
 
     test('should not be applied discount',()=>{
-        expect(inventory.calculateDiscount(-0.5,0.2)).toBe(`Price must be a positive number.`);
+        expect(inventory.calculateDiscount(-0.5,0.2)).toBe(`Price must be a positive number.`);//negative test case
     });
 
     test('should not be applied discount',()=>{
@@ -42,6 +42,9 @@ describe('calculateDiscount',()=>{
     test('Invalid inputs given ,throws error.',()=>{
         expect(inventory.calculateDiscount('100','0.2')).toBe(`Price and Discount must be a number.`);
     });
+    test('0% discount returns same price',()=>{
+        expect(inventory.calculateDiscount(100,0)).toBe(100);//edge case
+    });
 
 });
 
@@ -49,7 +52,7 @@ describe('calculateDiscount',()=>{
 describe('The filterProducts() function', () => {
     test('should remove products that do not meet the criteria', () => {
         expect(inventory.filterProducts(sortTest, product => product.amount > 4)).toEqual(sortSol)
-    }); 
+    }); //positive
     
     test('does not filter products that are not an Array', () => {
         expect(inventory.filterProducts(indProd,/*Any Callback*/)).toBe(`Please use a valid array and filter`)
@@ -58,6 +61,10 @@ describe('The filterProducts() function', () => {
     test('should throw an error for an input that is not a function', () => {
         expect(inventory.filterProducts(sortTest,"amount > 4")).toBe(`Please use a valid array and filter`)
     });
+
+    test('returns empty array when there is no product match', () => {
+        expect(inventory.filterProducts(sortTest, product => product.amount > 200)).toEqual([])
+    }); //edge case
 });
 
 
@@ -75,9 +82,9 @@ describe("sortInventory function", function(){
     expect(inventory.sortInventory(sortTest,"quantity")).toEqual(sortTest);
     });
 
-    test("returns empty array for invalid inputs ", function(){
-    expect(inventory.sortInventory(null,"amount")).toEqual([]);
-    });
+    test("returns empty array for empty array is inputs ", function(){
+    expect(inventory.sortInventory([],"amount")).toEqual([]);
+    });//edge case
 
     test("returns empty array for invalid inputs ", function(){
     expect(inventory.sortInventory(sortTest,null)).toEqual([]);
